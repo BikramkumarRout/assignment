0830d0d5788de740e04499d0ab92ed16665230fc
Bikram <bikramrout24@gmail.com>
2020-11-29 15:54:38 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SpacexComponent } from './spacex/spacex.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'spacex', pathMatch: 'prefix'},
+  { path: 'spacex' , component: SpacexComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
